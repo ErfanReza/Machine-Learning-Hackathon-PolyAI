bafedec1ebddb87b956e864a87e2a8139371bd1b
Erfan Reza <erfanreza14@gmail.com>
2023-10-12 14:11:00 -0400

    Create ML.py

---
+++ ML.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+# Load the dataset
+data = pd.read_csv('data_participant.csv')
+
+# Extract features and labels
+X = data.drop('target_column', axis=1)
+y = data['target_column']
+
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Scale/normalize features if needed
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+model = LogisticRegression()
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
+print(f"Accuracy: {accuracy}")
